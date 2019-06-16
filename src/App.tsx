c798ca65ad32bec1913e723d8fa4be129ff141ec
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-16 17:58:57 +0900

    AppからTodosとPreviewを呼び出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Todos from './components/Todos';
+import Preview from './components/Preview';
+
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todos />
+      <Preview />
+    </div>
+  );
 };
 
 export default App;
