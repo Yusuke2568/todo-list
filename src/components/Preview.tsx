51b3e85cb826d212fb34b193e668d875446b539e
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-27 15:30:43 +0900

    textをマークダウンのテキストで表示するように修正

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Marked } from 'marked-ts';
 import { Todo as ITodo } from '../reducer';
 
 interface Props {
@@ -6,12 +7,18 @@ interface Props {
 }
 
 const Preview: React.FC<Props> = ({ previewTodo }) => {
+  const previewText = previewTodo ? previewTodo.body : '- こんにちは';
+
   return (
     <div>
       <h3>タイトル</h3>
       {previewTodo ? previewTodo.title : null}
       <h4>テキスト</h4>
-      {previewTodo ? previewTodo.body : null}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: Marked.parse(previewText),
+        }}
+      ></div>
     </div>
   );
 };
