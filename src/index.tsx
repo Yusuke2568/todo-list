2caa5c671703932105db33361a1d2697f8750710
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-28 13:47:45 +0900

    redux-persistを使う方針に戻した

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './sagas';
+
+// redux-sagaを使うときはコメントアウトを外す
+// import createSagaMiddleware from 'redux-saga';
+// import rootSaga from './sagas';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // redux-persist を使って、store をlocalstorageで管理する様にする
 // だからここでは reducer を importしない
-import TodosReducer from './reducer';
+// import TodosReducer from './reducer';
 // import { createStore } from 'redux';
-// import store, { persistor } from './configureStore';
+import store, { persistor } from './configureStore';
 
 // redux-persist でlocalstorageに保存した store を使うため、ここでcreateStoreはしない
+// redux-sagaもredux-persistも使わない時に使う
 // const store = createStore(TodosReducer);
 
-const sagaMiddleWare = createSagaMiddleware();
-const store = createStore(TodosReducer, applyMiddleware(sagaMiddleWare));
+// redux-sagaを使うときはコメントアウトを外す
+// const sagaMiddleWare = createSagaMiddleware();
+// const store = createStore(TodosReducer, applyMiddleware(sagaMiddleWare));
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
@@ -36,4 +40,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-sagaMiddleWare.run(rootSaga);
+
+// for redux-saga
+// sagaMiddleWare.run(rootSaga);
