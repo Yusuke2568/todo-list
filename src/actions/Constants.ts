39a3ad60c3cdc2d90fc0df7f864f6713feffcf66
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-28 13:52:20 +0900

    constantsにDELETEを追加

--- src/actions/Constants.ts
+++ src/actions/Constants.ts
@@ -1,5 +1,6 @@
 export const ADD = 'ADD';
 export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
 export const START = 'START';
 export const SUCCESS = 'SUCCESS';
 export const FAIL = 'FAIL';
