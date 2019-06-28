6ebc02c36e64cc3c413abb7a97bf16914fefd186
Yusuke2568 <yuuuuuuusuke1215@gmail.com>
2019-06-28 14:03:29 +0900

    deleteではなくdestroyに修正

--- src/actions/Constants.ts
+++ src/actions/Constants.ts
@@ -1,6 +1,6 @@
 export const ADD = 'ADD';
 export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
+export const DESTROY = 'DESTROY';
 export const START = 'START';
 export const SUCCESS = 'SUCCESS';
 export const FAIL = 'FAIL';
